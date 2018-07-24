9de435f458ab73425f6a0aa63bd6e5c25df9a5f7
Victor Sharamet <sharamet@gmail.com>
2018-07-24 16:56:27 +0300

    gulptest commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync').create();
+
+var sass = require('gulp-sass');
+
+
+
+gulp.task('server', ['sass'], function() {
+    
+    browserSync.init({
+
+    	server: { baseDir: './app/'}
+
+    });
+
+    gulp.watch('./app/**/*.html').on('change', browserSync.reload);
+	
+	gulp.watch('./app/sass/**/*.scss', ['sass']);
+
+});
+
+gulp.task('sass', function() {
+
+    return gulp.src('./app/sass/**/*.scss')
+
+    .pipe(sass())
+
+    .pipe(gulp.dest('./app/css'))
+
+    .pipe(browserSync.stream());
+
+});
+
+gulp.task('default', ['server']);
+
+// gulp.task('copy',function(){
+// 	gulp.src('./app/index.html').pipe(gulp.dest('./dist'))
+// });
